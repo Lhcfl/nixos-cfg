92e90dfc3bb479beb27f18c8ea945b5a3e3d5f43
linca <lhcfl@outlook.com>
2025-12-26 22:33:29 +0800

    git config

--- home/linca/home.nix
+++ home/linca/home.nix
@@ -18,6 +18,9 @@
   programs.git = {
     enable = true;
     settings = {
+      init = {
+        defaultBranch = "main";
+      };
       user = {
         mame = "linca";
         email = "lhcfl@outlook.com";
