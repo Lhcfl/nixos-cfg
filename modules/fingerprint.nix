fdf860d580aaa0d4600522951c3e4201a31f57bf
linca <lhcfl@outlook.com>
2025-12-26 20:53:29 +0800

    add fingerprint by wiki
    https://wiki.nixos.org/wiki/Fingerprint_scanner

--- modules/fingerprint.nix
+++ modules/fingerprint.nix
@@ -1,8 +1,24 @@
-_: {
-  services.fprintd.enable = true;
+{ pkgs, ... }:
+{
+  # the driver
+  services.fprintd = {
+    enable = true;
+
+    # If simply enabling fprintd is not enough, try enabling fprintd.tod...
+    tod.enable = true;
+    # ...and use one of the next four drivers
+    # tod.driver = pkgs.libfprint-2-tod1-goodix; # Goodix driver module
+    tod.driver = pkgs.libfprint-2-tod1-elan; # Elan(04f3:0c4b) driver
+    # tod.driver = pkgs.libfprint-2-tod1-vfs0090; # (Marked as broken as of 2025/04/23!) driver for 2016 ThinkPads
+    # tod.driver = pkgs.libfprint-2-tod1-goodix-550a; # Goodix 550a driver (from Lenovo)
+
+    # however for focaltech 2808:a658, use fprintd with overidden package (without tod)
+    # package = pkgs.fprintd.override {
+    #   libfprint = pkgs.libfprint-focaltech-2808-a658;
+    # };
+  };
 
   security = {
-
     polkit.enable = true;
 
     polkit.extraConfig = ''
