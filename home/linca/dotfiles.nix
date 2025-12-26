822bd00af2f25b33f813956cba970f218f82deb8
linca <lhcfl@outlook.com>
2025-12-26 18:13:50 +0800

    add mako and helix config

--- home/linca/dotfiles.nix
+++ home/linca/dotfiles.nix
@@ -1,11 +1,13 @@
 { config, ... }:
 {
-  home.file = {
-    ".config/kitty" = {
-      source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/kitty;
+  home.file =
+    let
+      mkDotfilesLnk = name: config.lib.file.mkOutOfStoreSymlink ./dotfiles/${name};
+    in
+    {
+      ".config/kitty" = mkDotfilesLnk "kitty";
+      ".config/hypr" = mkDotfilesLnk "hypr";
+      ".config/helix" = mkDotfilesLnk "helix";
+      ".config/mako" = mkDotfilesLnk "mako";
     };
-    ".config/hypr" = {
-      source = config.lib.file.mkOutOfStoreSymlink ./dotfiles/hypr;
-    };
-  };
 }
