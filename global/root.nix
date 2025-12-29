e5ffbf1985f7303bc47271b55e4f3cad8cd1a4ba
linca <lhcfl@outlook.com>
2025-12-29 11:58:49 +0800

    chang root shell

--- global/root.nix
+++ global/root.nix
@@ -1,5 +1,14 @@
 { pkgs, ... }:
 {
+  programs = {
+    zsh.enable = true;
+    zoxide.enable = true;
+    starship.enable = true;
+    starship.presets = [
+      "plain-text-symbols"
+    ];
+  };
+
   users.users.root = {
     shell = pkgs.zsh;
   };
