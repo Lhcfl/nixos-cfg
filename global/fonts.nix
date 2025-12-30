03341dd0cf18c65769748716c72134056a86994e
linca <lhcfl@outlook.com>
2025-12-30 23:16:32 +0800

    font: make noto sans the default

--- global/fonts.nix
+++ global/fonts.nix
@@ -14,5 +14,35 @@
       noto-fonts-color-emoji
       lxgw-wenkai-screen
     ];
+
+    fontconfig.defaultFonts = {
+      sansSerif = [
+        "Noto Sans"
+        "Noto Sans CJK SC"
+        "Noto Sans CJK TC"
+        "Noto Sans CJK JP"
+        "Noto Sans CJK KR"
+        "Noto Color Emoji"
+      ];
+
+      serif = [
+        "Noto Serif"
+        "Noto Serif CJK SC"
+        "Noto Serif CJK TC"
+        "Noto Serif CJK JP"
+        "Noto Serif CJK KR"
+        "Noto Color Emoji"
+      ];
+
+      monospace = [
+        "Maple Mono NF CN"
+        "Noto Sans Mono"
+        "Noto Color Emoji"
+      ];
+
+      emoji = [
+        "Noto Color Emoji"
+      ];
+    };
   };
 }
