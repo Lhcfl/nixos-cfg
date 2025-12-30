ef87d0cad76e8aa493f0853bf57f480fc8057372
linca <lhcfl@outlook.com>
2025-12-30 10:47:13 +0800

    add github.com seperate identity file

--- home/linca/programs/ssh.nix
+++ home/linca/programs/ssh.nix
@@ -8,6 +8,12 @@ _: {
         serverAliveInterval = 30;
         userKnownHostsFile = "~/.ssh/known_hosts";
       };
+      "github.com" = {
+        user = "Lhcfl";
+        hostname = "github.com";
+        identityFile = "~/.ssh/id_ed25519.github";
+        addKeysToAgent = "yes";
+      };
     };
   };
 }
