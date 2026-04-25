1d65f439167ea88da7185e9e41297d4c219c922c
linca <lhcfl@outlook.com>
2026-04-25 15:46:43 +0800

    make tlp defaultly balanced

--- modules/laptop.nix
+++ modules/laptop.nix
@@ -12,7 +12,15 @@
 
   config = lib.mkIf config.funkcia.modules.laptop.enable {
     powerManagement.enable = false;
-    services.tlp.enable = true; # tlp is a good power management tool for laptops
-    services.tlp.pd.enable = true;
+    services.tlp = {
+      enable = true; # tlp is a good power management tool for laptops
+      pd.enable = true;
+
+      settings = {
+        TLP_PROFILE_AC = "BAL";
+        PLATFORM_PROFILE_ON_AC = "balanced";
+        TLP_PROFILE_BAT = "SAV";
+      };
+    };
   };
 }
