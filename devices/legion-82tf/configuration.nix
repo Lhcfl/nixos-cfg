d17c9617ae845f433433e22b7d95cd8bf514e703
linca <lhcfl@outlook.com>
2026-05-28 23:47:18 +0800

    flake update and change ly bkg

--- devices/legion-82tf/configuration.nix
+++ devices/legion-82tf/configuration.nix
@@ -24,7 +24,14 @@
 
   services.displayManager.ly = {
     settings = {
-      animation = "gameoflife";
+      animation = "dur_file";
+      dur_file_path = toString (
+        pkgs.fetchurl {
+          url = "https://codeberg.org/attachments/f336d6ac-8331-4323-91fc-0e4619803401";
+          hash = "sha256-fRm0wlkq9/GdLrVBOzMEnQG/i2ng+uGIzq0u9hu3m9g=";
+        }
+      );
+      full_color = true;
     };
     enable = true;
   };
