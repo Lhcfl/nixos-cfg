c82eaa9d088222756fa216aed20c2eb4bf41c393
linca <lhcfl@outlook.com>
2026-08-07 22:17:07 +0800

    add noctalia v5 state sync

--- home/linca/sync/sync.ts
+++ home/linca/sync/sync.ts
@@ -5,28 +5,38 @@ import path from "node:path";
 const here = import.meta.dirname;
 
 const dir = path.join(here, "Config/");
-for (const config of await fs.readdir(dir)) {
-  const src = path.join(dir, config);
-  const dist = path.join(homedir(), ".config", config)
-  console.log("linking", config, "from", src, "->", dist);
+
+const DIR_MAP = {
+  "Config/hypr": ".config/hypr",
+  "Config/nvim": ".config/nvim",
+  "Config/vicinae": ".config/vicinae",
+
+  "State/noctalia/settings.toml": ".local/state/noctalia/settings.toml",
+}
+
+for (const [source, dest] of Object.entries(DIR_MAP).map(([source, dest]) => [
+  path.join(here, source),
+  path.join(homedir(), dest)
+] as const)) {
+  console.log("linking", source, "from", source, "->", dest);
 
   try {
-    const x = await fs.lstat(dist);
+    const x = await fs.lstat(dest);
     if (x.isSymbolicLink()) {
-      const origin = await fs.readlink(dist);
+      const origin = await fs.readlink(dest);
       if (
-        path.normalize(src) == path.normalize(origin)
+        path.normalize(source) == path.normalize(origin)
       ) {
         console.log("success.")
         continue;
       }
 
-      console.error("exists a symlink: ", dist, "->", origin)
+      console.error("exists a symlink: ", dest, "->", origin)
     }
 
-    console.error("exists a file: ", dist)
+    console.error("exists a file: ", dest)
   } catch {
-    await fs.symlink(src, dist).catch((err) => console.error(err))
+    await fs.symlink(source, dest).catch((err) => console.error(err))
   }
 }
 
