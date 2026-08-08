47a471a1a9344873c37bd69aeea70dd6833aa494
linca <lhcfl@outlook.com>
2026-08-08 18:37:23 +0800

    fix qq in wayland

--- home/linca/programs/gui/qq.nix
+++ home/linca/programs/gui/qq.nix
@@ -3,18 +3,10 @@
 { lib, pkgs, ... }: {
   home.packages = [
     (pkgs.qq.overrideAttrs (
-      final: prev:
-      let
-        extra-flag = ''--add-flags "\''${NIXOS_OZONE_WL:+\''${WAYLAND_DISPLAY:+--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true --wayland-text-input-version=3 --enable-features=UseOzonePlatform --ozone-platform=wayland}}" \'';
-
-        splitLines = lib.strings.splitString "\n" prev.installPhase;
-
-        addExtraLine = map (
-          line: if lib.strings.hasPrefix "makeShellWrapper" line then line + "\n  " + extra-flag else line
-        ) splitLines;
-      in
-      {
-        installPhase = lib.strings.concatStringsSep "\n" addExtraLine;
+      final: prev: {
+        installPhase =
+          lib.replaceStrings [ "--ozone-platform-hint=auto" ] [ "--ozone-platform=wayland" ]
+            prev.installPhase;
       }
     ))
   ];
