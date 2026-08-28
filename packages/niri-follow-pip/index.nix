6c5c1882de40fc6c96219b334068ba966759d809
linca <lhcfl@outlook.com>
2026-08-28 20:37:14 +0800

    rewrite show-tray-items

--- packages/niri-follow-pip/index.nix
+++ packages/niri-follow-pip/index.nix
@@ -1,9 +1,13 @@
 { ... }:
-let name = "niri-follow-pip"; in
+let
+  name = "niri-follow-pip";
+in
 {
   perSystem = { pkgs, config, ... }: {
-    packages.${name} = (pkgs.writers.writeNuBin name ./app.nu).overrideAttrs (_: {
-      meta.description = "niri 画中画 (Picture In Picture) 窗口自动跟随 workspace";
+    packages.${name} = (pkgs.writers.writeNuBin name ./app.nu).overrideAttrs (old: {
+      meta = old.meta // {
+        description = "niri 画中画 (Picture In Picture) 窗口自动跟随 workspace";
+      };
     });
     overlayAttrs.${name} = config.packages.${name};
   };
--- packages/show-tray-items/index.nix
+++ packages/show-tray-items/index.nix
@@ -1,9 +1,13 @@
 { ... }:
-let name = "show-tray-items"; in
+let
+  name = "show-tray-items";
+in
 {
   perSystem = { pkgs, config, ... }: {
-    packages.${name} = (pkgs.writers.writeNuBin name ./app.nu).overrideAttrs (_: {
-      meta.description = "给出任务栏图标";
+    packages.${name} = (pkgs.writers.writeNuBin name ./app.nu).overrideAttrs (old: {
+      meta = old.meta // {
+        description = "给出任务栏图标";
+      };
     });
     overlayAttrs.${name} = config.packages.${name};
   };
