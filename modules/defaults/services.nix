fc72710a6091faae91a7495bf8957ae7715b51b8
linca <lhcfl@outlook.com>
2026-08-30 14:34:07 +0800

    remove broker, as broker is default in 26.05

--- modules/defaults/services.nix
+++ modules/defaults/services.nix
@@ -1,9 +1,6 @@
 _: {
   # List services that you want to enable:
   services = {
-    # faster new tech
-    dbus.implementation = "broker";
-
     # use chrony instead of timesyncd for better time synchronization
     chrony.enable = true;
     timesyncd.enable = false;
