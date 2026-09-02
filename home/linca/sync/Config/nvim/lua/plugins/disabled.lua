b6336ac05e5621f77095c427e443b95d2419b39c
linca <lhcfl@outlook.com>
2026-09-02 21:57:06 +0800

    update disabled.lua

--- home/linca/sync/Config/nvim/lua/plugins/disabled.lua
+++ home/linca/sync/Config/nvim/lua/plugins/disabled.lua
@@ -1,5 +1,12 @@
+local handle = io.popen("uname -a")
+local sysinfo = handle:read("*a")
+handle:close()
+
+local is_nixos = string.find(sysinfo, "NixOS", 1) ~= nil
+
 return {
-  { "mason-org/mason-lspconfig.nvim", enabled = false },
-  { "mason-org/mason.nvim",           enabled = false },
-  { "mpeterv/hererocks.nvim", enabled = false}
+  { "mason-org/mason-lspconfig.nvim", enabled = ! is_nixos },
+  { "mason-org/mason.nvim",           enabled = ! is_nixos },
+  { "mpeterv/hererocks.nvim",         enabled = ! is_nixos }
 }
+
