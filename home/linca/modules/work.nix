97e2095feb278be1bab0f0f8d5d45c0edad20537
linca <lhcfl@outlook.com>
2026-09-03 14:38:41 +0800

    pandoc: specify pdf-engine typst

--- home/linca/modules/work.nix
+++ home/linca/modules/work.nix
@@ -22,7 +22,16 @@ in
       typst
     ];
 
-    programs.pandoc.enable = true;
+    # pandoc 导出 PDF：默认引擎是 pdflatex（需 texlive），这里改用已安装的 typst。
+    # 注意 pandoc 的 typst 模板默认 font 为空，需通过 mainfont 变量指定字体。
+    programs.pandoc = {
+      enable = true;
+      defaults = {
+        pdf-engine = "typst";
+        variables.mainfont = "Noto Serif";
+      };
+    };
+
     programs.direnv.enable = true;
   };
 }
