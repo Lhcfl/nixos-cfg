2ea5bbc443d0d5ab4c66470087ff1f856613a733
linca <lhcfl@outlook.com>
2026-09-03 16:16:05 +0800

    add deepseek api key secret

--- home/linca/programs/pi-coding-agent.nix
+++ home/linca/programs/pi-coding-agent.nix
@@ -5,9 +5,33 @@
   ...
 }:
 {
-  config = lib.mkIf (config.linca.work.enable) {
-    home.packages = with pkgs; [
-      pi-coding-agent
-    ];
-  };
+  config = lib.mkIf (config.linca.work.enable) (
+    lib.mkMerge [
+      {
+        home.packages = with pkgs; [
+          pi-coding-agent
+        ];
+      }
+
+      (lib.mkIf (config.linca.sops.enable) (
+        let
+          template-name = "pi-auth-json";
+        in
+        {
+          sops.secrets.deepseek-api-key = { };
+
+          sops.templates.${template-name}.content = builtins.toJSON {
+            deepseek = {
+              type = "api_key";
+              key = config.sops.placeholder."deepseek-api-key";
+            };
+          };
+
+          home.file.".pi/agent/auth.json".source =
+            config.lib.file.mkOutOfStoreSymlink
+              config.sops.templates.${template-name}.path;
+        }
+      ))
+    ]
+  );
 }
