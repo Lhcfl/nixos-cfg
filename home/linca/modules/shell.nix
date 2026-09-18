258b954f4404007dc6bf1430be439b2d161edbfe
linca <lhcfl@outlook.com>
2026-09-18 19:51:25 +0800

    ✨ linca (shell): 用 shell-auto-pi 处理 fish 命令未找到

--- home/linca/modules/shell.nix
+++ home/linca/modules/shell.nix
@@ -1,5 +1,15 @@
+{ pkgs, lib, inputs, ... }:
+let
+  shell-auto-pi = inputs.shell-auto-pi.packages.${pkgs.stdenv.hostPlatform.system}.default;
+in
 {
-  programs.fish.shellAliases = {
-    nd = "nix develop -c $SHELL";
+  programs.fish = {
+    shellAliases = {
+      nd = "nix develop -c $SHELL";
+    };
+
+    functions.fish_command_not_found = ''
+      ${lib.getExe shell-auto-pi} auto $argv
+    '';
   };
 }
