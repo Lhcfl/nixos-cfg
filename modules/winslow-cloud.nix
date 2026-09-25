2fe1711b79cb27d160218d590c224764369803d2
linca <lhcfl@outlook.com>
2026-09-25 21:54:09 +0800

    test

--- modules/winslow-cloud.nix
+++ modules/winslow-cloud.nix
@@ -67,8 +67,8 @@
 
         # 3. Optimization: Prevent systemd from waiting for network online
         # (Optional but recommended for faster boot with VPNs)
-        systemd.network.wait-online.enable = false;
-        boot.initrd.systemd.network.wait-online.enable = false;
+        # systemd.network.wait-online.enable = false;
+        # boot.initrd.systemd.network.wait-online.enable = false;
       })
     ]
   );
