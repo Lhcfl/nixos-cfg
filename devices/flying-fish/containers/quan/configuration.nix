c8a40d0f6d2e25893e225314f0b77aa99f58cd0b
linca <lhcfl@outlook.com>
2026-09-27 01:58:02 +0800

    🔒 flying-fish/quan: 为 linca 添加 wheel 组并限制 SSH 允许用户

--- devices/flying-fish/containers/quan/configuration.nix
+++ devices/flying-fish/containers/quan/configuration.nix
@@ -14,8 +14,11 @@
   users.users.linca = {
     isNormalUser = true;
     initialPassword = "change-me-now";
+    extraGroups = [ "wheel" ];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJwHaPGjtqvGsYrO5NiGHoVMSS/Qj+63hv1QNBG+wnm+ linca@nixos"
     ];
   };
+
+  services.openssh.settings.AllowUsers = [ "linca" ];
 }
